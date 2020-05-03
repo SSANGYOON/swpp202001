e9ee46eac9c4d29e7d7a3d5be96908b9b411541b
KHJ <nanjin96@snu.ac.kr>
2020-05-04 08:41:29 +0900

    Skeleton code for packing pass.

---
+++ src/mem_packing/packing.h
@@ -0,0 +1,47 @@
+#ifndef TEAM13_PACKING_H
+#define  TEAM13_PACKING_H
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include <string>
+
+#include "llvm/IR/Value.h"
+
+enum PackedPosition = {HIGHER,LOWER};
+
+class Packing{
+  llvm::Value *packingReg; //packing register after replacement for assignment
+  llvm::Value *memValue;  //packed variable's Value instance.
+  enum PackedPosition packedPos; //position in packing register
+public:
+  llvm::Value getPackingReg(){return this->packingReg;}
+  llvm::Value getMemValue(){return this->memValue;}
+  llvm::Value getPackedPosition(){return this->packedPos;}
+
+  llvm::Value setPackingReg(llvm::Value* v){
+    this->packingReg = v;
+    return this->packingReg;
+  }
+  llvm::Value setMemValue(llvm::Value* v){
+    this->memValue = v;
+    return this->memValue;
+  }
+  llvm::Value setPackedPosition(enum PackedPosition pos){
+    this->packedPos = pos;
+    return this->packedPos;
+  }
+};
+
+class PackMemIntoReg : public llvm::PassInfoMixin<PackMemIntoReg> {
+  std::string outputFile;
+  bool printDepromotedModule;
+
+public:
+  PackMemIntoReg(std::string outputFile, bool printDepromotedModule) :
+      outputFile(outputFile), printDepromotedModule(printDepromotedModule) {}
+  llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM);
+};
+
+#endif
